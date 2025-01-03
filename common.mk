7e42b4ad7d94914855130ae33534e6a2a0faee6e
jmpfbmx <jmpf.bmx@gmail.com>
2025-01-03 14:01:36 +0100

    sm6375-common: Initial skeleton common tree
    Change-Id: I1b5dc05c8cc3e826c7c695e5c3d006349578d881
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

---
+++ common.mk
@@ -0,0 +1,16 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Shipping API
+BOARD_SHIPPING_API_LEVEL := 33
+PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/sony
+
+# Inherit from the proprietary files makefile.
+$(call inherit-product, vendor/sony/sm6375-common/sm6375-common-vendor.mk)
