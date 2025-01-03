3b7107da6116a5e3d7ad23ae426143d37bd01742
jmpfbmx <jmpf.bmx@gmail.com>
2025-01-03 14:06:41 +0100

    sm6375-common: Add kernel flags
    Change-Id: I3344aae7db46cd9c3fb8849ab5073184bfe5d685
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,6 +21,33 @@ TARGET_2ND_CPU_VARIANT := cortex-a75
 TARGET_BOOTLOADER_BOARD_NAME := holi
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=4e00000.dwc3 \
+    cgroup.memory=nokmem,nosocket \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    pcie_ports=compat \
+    service_locator.enable=1 \
+    swiotlb=0 \
+    ip6table_raw.raw_before_defrag=1 \
+    iptable_raw.raw_before_defrag=1
+BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_RAMDISK_USE_LZ4 := true
+TARGET_KERNEL_SOURCE := kernel/sony/sm6375
+TARGET_KERNEL_NO_GCC := true
+
+# Kernel modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := holi
