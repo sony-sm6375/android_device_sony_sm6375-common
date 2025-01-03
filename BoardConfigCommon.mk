8099e300dd8d4b74c604f7c2ef426b4a04806e07
jmpfbmx <jmpf.bmx@gmail.com>
2025-01-03 14:03:25 +0100

    sm6375-common: Add arch, bootloader and platform flags
    Change-Id: Ie0ca1d06a57a715867ceb4954cc37248cd099081
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,5 +5,25 @@
 
 COMMON_PATH := device/sony/sm6375-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := kryo385
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a75
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := holi
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := holi
+
 # Include the proprietary files BoardConfig.
 include vendor/sony/sm6375-common/BoardConfigVendor.mk
