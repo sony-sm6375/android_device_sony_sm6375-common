9d3d4210ea4b81ca75d369cc56b5c383f9fc9453
jmpfbmx <jmpf.bmx@gmail.com>
2025-01-03 14:01:36 +0100

    sm6375-common: Initial skeleton common tree
    Change-Id: I1b5dc05c8cc3e826c7c695e5c3d006349578d881
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/sony/sm6375-common
+
+# Include the proprietary files BoardConfig.
+include vendor/sony/sm6375-common/BoardConfigVendor.mk
