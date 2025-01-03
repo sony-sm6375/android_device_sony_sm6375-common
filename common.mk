0d362d53aa15b59f51ebb1b0292b338848286d0f
Daniel Rosenberg <drosen@google.com>
2025-01-03 14:18:34 +0100

    sm6375-common: Enable casefolding support on userdata
    This enables casefolding, which is required for removal of sdcardfs.
    
    Test: Wipe data and boot. lsattr /data/media, check for +F
    Bug: 138322712
    Change-Id: I020aa6bb9c121e7bda83c07bc9fb37dcfa26702c

--- common.mk
+++ common.mk
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := false
 
