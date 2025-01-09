4f7af67226b5663c8112d97d352e5d9da796585d
LuK1337 <priv.luk@gmail.com>
2025-01-09 22:33:03 +0100

    sm6375-common: Include hardware/sony sepolicy
    Change-Id: I51fbdc08f2aa53d47da571b1daee53780be203de

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,6 +96,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+include hardware/sony/sepolicy/qti/SEPolicy.mk
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
