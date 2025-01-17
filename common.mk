5e2ce4c93942c8e7217ee7356060ceb9dec6ba66
LuK1337 <priv.luk@gmail.com>
2025-01-17 21:58:23 +0100

    sm6375-common: Build FM2
    Change-Id: I37ae0ceb069121a31cfe7f7ede573e1af1c4abd4

--- common.mk
+++ common.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# FM
+PRODUCT_PACKAGES += \
+    FM2
+
 # GPS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
